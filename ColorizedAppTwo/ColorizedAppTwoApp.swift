1356fde4ff973dfaa50c512ed3872b45a9dbfeae
tatanadubova <tatyana.dubova.1986@gmail.com>
2023-12-15 13:37:09 +0300

    Initial Commit

---
+++ ColorizedAppTwo/ColorizedAppTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppTwoApp.swift
+//  ColorizedAppTwo
+//
+//  Created by Татьяна Дубова on 15.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
